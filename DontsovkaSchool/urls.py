8149e2795db9f38c24085b9eb893bfee477d937e
Dmitriy <83923541+Dmitriycom@users.noreply.github.com>
2023-10-20 22:50:44 +0300

    Create navBar
    Create navBar, default page. link and animation mouse-over

--- DontsovkaSchool/urls.py
+++ DontsovkaSchool/urls.py
@@ -9,4 +9,12 @@ from main import views
 urlpatterns = [
     re_path(r'admin/', admin.site.urls),
     re_path(r'^$', views.index, name='home'),
+    re_path(r'^about$', views.about, name='about'),
+    re_path(r'^students$', views.students, name='students'),
+    re_path(r'^parents$', views.parents, name='parents'),
+    re_path(r'^teachers$', views.teachers, name='teachers'),
+    re_path(r'^educProc$', views.educProc, name='educProc'),
+    re_path(r'^educProc2$', views.educProc2, name='educProc2'),
+    re_path(r'^news$', views.news, name='news'),
+    re_path(r'^mail$', views.mail, name='mail'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about.html")
+
+def students(request):
+    return render(request, "main/students.html")
+
+def parents(request):
+    return render(request, "main/parents.html")
+
+def teachers(request):
+    return render(request, "main/teachers.html")
+
+def educProc(request):
+    return render(request, "main/educProc.html")
+
+def educProc2(request):
+    return render(request, "main/educProc2.html")
+
+def news(request):
+    return render(request, "main/news.html")
+
+def mail(request):
+    return render(request, "main/mail.html")
